02c6ff650b8b6c1561f0a5abcedd4e7c6c61293a
Ticia <ticiarodela@gmail.com>
2020-05-14 16:49:36 -0700

    save

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
